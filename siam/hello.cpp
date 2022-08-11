f9ca5c9c77a7146d8f52b2e8e9ade9e571fa02cc
Ridwanur Rashid Siam <74427216+Ridwan60@users.noreply.github.com>
2022-08-11 14:41:41 +0600

    Update hello.cpp

--- siam/hello.cpp
+++ siam/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main() 
 {
 	std::cout << "Hello world" << endl;
-	std::cout << "This is Siam" << endl;
+	std::cout << "This is RidwanSiam" << endl;
 	cout << "Change1" << endl;
 	return 0;
 }
